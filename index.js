f500547ee851cc5c6fb739fe417343d2487d2d48
Ricardo Tovar <tricardo003@gmail.com>
2023-07-05 00:01:38 -0500

    get location

--- index.js
+++ index.js
@@ -1,62 +1,29 @@
 import express from "express";
-import { SuperfaceClient } from "@superfaceai/one-sdk";
 const app = express();
 import axios from 'axios';
- 
+
 app.set("trust proxy", true);
 
-const sdk = new SuperfaceClient();
-
-async function run(ip) {
-    // Load the profile
-    const profile = await sdk.getProfile("address/ip-geolocation@1.0.1");
-
-    // Use the profile
-    const result = await profile.getUseCase("IpGeolocation").perform(
-        {
-            ipAddress: ip
-        },
-        {
-            provider: "ipdata",
-            security: {
-                apikey: {
-                    apikey: process.env.API_DATA
-                }
-            }
-        }
-    );
-
-    // Handle the result
-    try {
-        const data = result.unwrap();
-        console.log('data',data);
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
-}
 
 app.get("/", async (req, res) => {
     const ip = req.ip;
-    console.log(ip);
-const apiKey = 'b79bbfc5e7a6c5a1f26a82f7261125ee'; // Obtiene la clave de API desde un archivo .env
+    const apiKey = process.env.IPSTACK_API_KEY; // Obtiene la clave de API https://ipstack.com/dashboard desde un archivo .env
+    const url = `http://api.ipstack.com/${ip}?access_key=${apiKey}`;
 
-const url = `http://api.ipstack.com/${ip}?access_key=${apiKey}`;
+    try {
+        const response = await axios.get(url);
+        const { latitude, longitude } = response.data;
+        const url_geocode_client = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+        const response_accuracy = await axios.get(url_geocode_client);
+        res.send({
+            ...response_accuracy.data,
+            ip
+        });
+    } catch (error) {
+        console.log('error', error);
+        res.status(500).send('Error al obtener la ubicación');
+    }
 
-axios.get(url)
-.then((response) => {
-  const { country_name, region_name, city, latitude, longitude } = response.data;
-
-  console.log('País:', country_name);
-  console.log('Región:', region_name);
-  console.log('Ciudad:', city);
-  console.log('Latitud:', latitude);
-  console.log('Longitud:', longitude);
-})
-.catch((error) => {
-  console.error('Error al obtener la ubicación:', error.message);
-});
-    res.send(await run(req.ip));
 });
 
 app.listen(3000, () => {
