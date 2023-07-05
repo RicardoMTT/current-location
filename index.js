7f5b9df083880522cd13aceb686eb21f5b49dc56
Ricardo Tovar <tricardo003@gmail.com>
2023-07-04 23:30:24 -0500

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { SuperfaceClient } from "@superfaceai/one-sdk";
 const app = express();
+import axios from 'axios';
+ 
 app.set("trust proxy", true);
 
 const sdk = new SuperfaceClient();
@@ -35,6 +37,25 @@ async function run(ip) {
 }
 
 app.get("/", async (req, res) => {
+    const ip = req.ip;
+    console.log(ip);
+const apiKey = 'b79bbfc5e7a6c5a1f26a82f7261125ee'; // Obtiene la clave de API desde un archivo .env
+
+const url = `http://api.ipstack.com/${ip}?access_key=${apiKey}`;
+
+axios.get(url)
+.then((response) => {
+  const { country_name, region_name, city, latitude, longitude } = response.data;
+
+  console.log('País:', country_name);
+  console.log('Región:', region_name);
+  console.log('Ciudad:', city);
+  console.log('Latitud:', latitude);
+  console.log('Longitud:', longitude);
+})
+.catch((error) => {
+  console.error('Error al obtener la ubicación:', error.message);
+});
     res.send(await run(req.ip));
 });
 
